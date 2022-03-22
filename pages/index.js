b126a41216d033d56a49f9148c067d93f2df3960
Ross carmack <rosscarmack@Rosss-MBP.lan>
2022-03-22 10:39:12 -0500

    added footer and fixed image in medium card

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Header from '../components/Header'
 import SmallCard from '../components/SmallCard'
 import MediumCard from '../components/MediumCard'
 import LargeCard from '../components/LargeCard'
+import Footer from '../components/Footer'
 
 
 export default function Home({ exploreData, cardsData }) {
@@ -56,6 +57,8 @@ export default function Home({ exploreData, cardsData }) {
           buttonText='Get Inspired'
         />
       </main>
+      
+      <Footer />
 
     </div>
   )
