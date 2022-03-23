160f8ca559128e7e848219d0fae7f6b4e85c7298
Ross carmack <rosscarmack@Rosss-MBP.lan>
2022-03-23 12:24:49 -0500

    added query params and used them

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import { SearchIcon, MenuIcon, UserCircleIcon, UsersIcon, GlobeAltIcon } from '@heroicons/react/solid'
 import { DateRangePicker } from 'react-date-range'
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
 
 function Header() {
+
+    const router = useRouter()
 
     const [searchInput, setSearchInput] = useState('')
     const [startDate, setStartDate] = useState(new Date())
@@ -32,13 +35,25 @@ function Header() {
         setSearchInput('')
     }
 
+    const searchHandler = () => {
+        router.push({
+            pathname: '/search',
+            query: {
+                location: searchInput,
+                startDate: startDate.toISOString(),
+                endDate: endDate.toISOString(),
+                numOfGuests: numOfGuests
+            }
+        })
+    }
+
    
   
 
     return (
         <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
            
-            <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+            <div className='relative flex items-center h-10 cursor-pointer my-auto' onClick={() => router.push('/')}>
                 <Image
                     src='https://links.papareact.com/qd3'
                     layout='fill'
@@ -92,7 +107,7 @@ function Header() {
                     </div>
                     <div className='flex'>
                         <button onClick={resetInput} className='flex-grow text-gray-500'>Cancel</button>
-                        <button className='flex-grow text-red-400'>Search</button>
+                        <button onClick={searchHandler} className='flex-grow text-red-400'>Search</button>
                     </div>
                 </div>
                 )}
