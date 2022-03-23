3624813f9a2f5f81205f24f130aaa63a90e67d62
Ross carmack <rosscarmack@Rosss-MBP.lan>
2022-03-23 10:22:12 -0500

    fixing globeicon location on mobile

--- components/Header.js
+++ components/Header.js
@@ -23,7 +23,7 @@ function Header() {
                 <SearchIcon 
                 className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2'/>
             </div>
-            <div className='flex space-x-4 items-center justify-end text-gray-500'>
+            <div className='flex space-x-1 sm:space-x-4 items-center justify-end text-gray-500'>
                 <p className='hidden md:inline cursor-pointer'>Become a host</p>
                 <GlobeAltIcon 
                     className='h-6 cursor-pointer'
