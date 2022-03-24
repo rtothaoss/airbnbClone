ff0c49e0e3baf0d70fa05e26e0e5181fded14e0d
Ross carmack <rosscarmack@Rosss-MBP.lan>
2022-03-24 11:44:48 -0500

    map up and running for desktop view. need to figure out solution for mobile view

--- components/Map.js
+++ components/Map.js
@@ -1,27 +1,64 @@
 import React, { useState } from 'react'
-import Map from 'react-map-gl'
+import Map, { Marker, Popup } from 'react-map-gl'
+import getCenter from 'geolib/es/getCenter'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
-function MapComponent() {
+function MapComponent({ searchResults }) {
+
+    const [selectedLocation, setSelectedLocation] = useState({})
+    // const [showPopup, setShowPopup] = useState(true)
+
+    const coordinates = searchResults.map(item => ({
+        longitude: item.long,
+        latitude: item.lat
+    }))
+
+    const center = getCenter(coordinates)
 
     const [viewState, setViewState] = useState({
         width: '100%',
         height: '100%',
-        longitude: -0.109889,
-        latitude: 51.521245,
+        longitude: center.longitude,
+        latitude: center.latitude,
         zoom: 11
     })
 
-  return (
-    <Map
-        mapStyle='mapbox://styles/rtothaoss/cl1555xs3000715qryrg2szvu'
-        mapboxAccessToken={process.env.mapbox_key}
-        {...viewState}
-        onMove={evt => setViewState(evt.viewState)}
-        
-    >
 
-    </Map>
-  )
+
+    return (
+        <Map
+            mapStyle='mapbox://styles/rtothaoss/cl1555xs3000715qryrg2szvu'
+            mapboxAccessToken={process.env.mapbox_key}
+            {...viewState}
+            onMove={evt => setViewState(evt.viewState)}
+        >
+            {searchResults.map((result) => (
+                <div key={result.long}>
+                    <Marker
+                        longitude={result.long}
+                        latitude={result.lat}
+                        anchor='center'
+                    >
+                        <p onClick={() => setSelectedLocation(result)} className='cursor-pointer text-2xl animate-bounce'>📌</p>
+                    </Marker>
+
+                    {selectedLocation.long === result.long ? (
+                        <Popup
+                            // onClose={() => setShowPopup(false)}
+                            closeOnClick={true}
+                            latitude={result.lat}
+                            longitude={result.long}
+                        >
+                            {result.title}
+                        </Popup>
+                    ) : false}
+
+                </div>
+            ))}
+
+
+        </Map>
+    )
 }
 
 export default MapComponent
